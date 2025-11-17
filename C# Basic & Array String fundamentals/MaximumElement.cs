d6533a1ad4c7480289845d5b4302d775b72d4eeb
Mayuresh Jadhav <mjm090972@gmail.com>
2025-11-18 00:59:24 +0530

    Add project files.

---
+++ C# Basic & Array String fundamentals/MaximumElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basic___Array_String_fundamentals
+{
+    public class MaximumElement
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Running Program 1...");
+            int[] array = new int[]{ 1, 2, 3, 4};
+            int max = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            Console.WriteLine($"Maximun element in array is:  {max}");
+        }
+    }
+}
---
+++ C# Basic & Array String fundamentals/Program.cs
@@ -0,0 +1,30 @@
+﻿using C__Basic___Array_String_fundamentals;
+using System;
+
+namespace BasicCsharp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Select program to run (1 - 15):");
+            Console.WriteLine("1. Find the maximum element in a given integer array.");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice) 
+            {
+                case 1:
+                    MaximumElement.Run();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
